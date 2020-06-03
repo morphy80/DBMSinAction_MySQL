6329e8a3c94a623738373fabdc786f72fbcfb627
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-06-03 16:46:52 +0000

    Delete-where-in command

--- mysql/mysql-from-python.py
+++ mysql/mysql-from-python.py
@@ -14,7 +14,12 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        rows = cursor.executemany("DELETE FROM Friends WHERE name = %s;", ['jim', 'fred'])
+        list_of_names = ['fred', 'fred', 'fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
         connection.commit()
 finally:
     # Close the conn., regardless of whether or not the above was successful
